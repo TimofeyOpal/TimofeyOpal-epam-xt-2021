b8cbe1fa25d2313c53c3926a733c923bdb1bc4ec
DESKTOP-9S501U7\MYLOCAL <timofey.gusev.opal@gmail.com>
2021-03-23 15:02:45 +0400

    Realized food

---
+++ Task 2/ConsoleGame_epam/ConsoleGame_epam/Food.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGame_epam
+{
+    class Food : Point
+    {
+        public Food()
+        {
+            Sprite = '@';
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+        public void Write(ref Wall wall)
+        {
+            Random r = new Random();
+            int y = r.Next(2, wall.Column);
+            int x = r.Next(2, wall.Row);       
+            Console.SetCursorPosition(x, y);
+            Console.Write(Sprite);
+        }
+    }
+}
--- Task 2/ConsoleGame_epam/ConsoleGame_epam/Program.cs
+++ Task 2/ConsoleGame_epam/ConsoleGame_epam/Program.cs
@@ -14,10 +14,19 @@ namespace ConsoleGame_epam
             Wall wall = new Wall(15, 15);
             wall.Drow();
             List<Enemy> koll = new List<Enemy>();
+            List<Food> food = new List<Food>();
             Console.CursorVisible = false;
             for (int i = 0; i < 4; i++)
             {
                 koll.Add(new Enemy());
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                food.Add(new Food());               
+            }
+            foreach (Food foodItem in food)
+            {
+                foodItem.Write(ref wall);
             }
             foreach (Enemy enemy in koll)
             {
